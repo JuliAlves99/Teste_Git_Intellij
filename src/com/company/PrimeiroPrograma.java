7729c4a31dd3b9db18715ec57d780cfcbc9de8c9
Juliana Alves <julianaalvess581@gmail.com>
2022-03-27 14:28:17 -0300

    first commit

---
+++ src/com/company/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import com.company.model.Gato;
+
+import javax.swing.*;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        //       CRIANDO A CLASSE GATO
+        Gato gato = new Gato("Bill", "laranja", 2);
+
+        System.out.println(gato);
+
+        //System.out.println("Hello, world!");
+        //JOptionPane.showMessageDialog(null, "Hello, world!");
+
+        /*int a=5, b=3;
+        System.out.println(a+b);*/
+
+    }
+}
